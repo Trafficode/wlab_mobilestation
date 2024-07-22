167fca25f08adf8bf38843ce2dadb12f570e0f35
Trafficode <kkuras89@gmail.com>
2024-07-22 08:04:18 +0200

    scripts for packet converting

--- scripts/mqtt.py
+++ scripts/mqtt.py
@@ -1,19 +1,75 @@
 import paho.mqtt.client as mqtt
+import struct
 
 # Define the MQTT broker details
 broker = "194.42.111.14"
 port = 1883
 topic = "/wlabdb/bin"
 
+def wlab_bin2dict(packet):
+    packet_dict = {
+        "version": packet[0],
+        "id": "%02X:%02X:%02X:%02X:%02X:%02X" % (packet[1], packet[2], packet[3], packet[4], packet[5], packet[6]),
+        "ts": struct.unpack('<q', packet[7:15])[0],
+        "temp_act": struct.unpack('<h', packet[15:17])[0],
+        "temp_avg": struct.unpack('<h', packet[17:19])[0],
+        "temp_max": struct.unpack('<h', packet[19:21])[0],
+        "temp_min": struct.unpack('<h', packet[21:23])[0],
+        "temp_max_ts_offset": struct.unpack('<h', packet[23:25])[0],
+        "temp_min_ts_offset": struct.unpack('<h', packet[25:27])[0],
+        "humidity_act": packet[27],
+        "humidity_avg": packet[28],
+        "humidity_max": packet[29],
+        "humidity_min": packet[30],
+        "humidity_max_ts_offset": struct.unpack('<h', packet[31:33])[0],
+        "humidity_min_ts_offset": struct.unpack('<h', packet[33:35])[0],
+        "battery_voltage": struct.unpack('<h', packet[35:37])[0],
+    }
+    return(packet_dict)
+
+def wlab_bin2old(packet_dict):
+    packet_wlab_dict = {
+        "UID": packet_dict["id"],
+        "TS": packet_dict["ts"],
+        "SERIE": {
+            "Temperature": {
+                "f_avg": "%.1f" % (packet_dict["temp_avg"]/10),
+                "f_act": "%.1f" % (packet_dict["temp_act"]/10),
+                "f_min": "%.1f" % (packet_dict["temp_min"]/10),
+                "f_max": "%.1f" % (packet_dict["temp_max"]/10),
+                "i_min_ts": packet_dict["ts"] + packet_dict["temp_min_ts_offset"],
+                "i_max_ts": packet_dict["ts"] + packet_dict["temp_max_ts_offset"],
+            },
+            "Humidity": {
+                "f_avg": "%.1f" % (packet_dict["temp_avg"]/10),
+                "f_act": "%.1f" % (packet_dict["temp_act"]/10),
+                "f_min": "%.1f" % (packet_dict["temp_min"]/10),
+                "f_max": "%.1f" % (packet_dict["temp_max"]/10),
+                "i_min_ts": packet_dict["ts"] + packet_dict["humidity_min_ts_offset"],
+                "i_max_ts": packet_dict["ts"] + packet_dict["humidity_max_ts_offset"],
+            }
+        }
+    }
+    return(packet_wlab_dict)
+
 # Callback function when a message is received
 def on_message(client, userdata, message):
-    print(f"Received message: {message.payload.decode()} on topic {message.topic}")
+    # print(f"Received message: {message.payload.decode()} on topic {message.topic}")
+    _dict = wlab_bin2dict(message.payload)
+    print(_dict)
+    wlab_old = wlab_bin2old(_dict)
+    print(wlab_old)
 
 # Callback function when connected to the broker
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected successfully to the broker")
         client.subscribe(topic)  # Subscribe to the topic
+        pub_packet = bytearray()
+        pub_packet.extend(b'\x01\x10\x21\x32\xa0\xfa\x03\x50\xd4\x12\x00\x00\x00\x00\x00')
+        pub_packet.extend(b'\x96\x00\x7c\x00\x44\x01\x20\xff\xe0\x00\x18\x00\x21\x00\x2d')
+        pub_packet.extend(b'\x0f\x55\x20\x00\x4d\x01\x70\x01')
+        client.publish(topic, pub_packet)
     else:
         print(f"Connect failed with code {rc}")
 
---
+++ scripts/wlab_bin2json.py
@@ -0,0 +1,83 @@
+import struct
+
+# \x01\x10\x21\x32\xa0\xfa\x03\x50\xd4\x12\x00\x00\x00\x00\x00
+# \x96\x00\x7c\x00\x44\x01\x20\xff\xe0\x00\x18\x00\x21\x00\x2d
+# \x0f\x55\x20\x00\x4d\x01\x70\x01
+
+# sample.version = 0x01;
+# sample.ts = 1234000;
+# sample.id[0] = 0x10;
+# sample.id[1] = 0x21;
+# sample.id[2] = 0x32;
+# sample.id[3] = 0xa0;
+# sample.id[4] = 0xfa;
+# sample.id[5] = 0x03;
+# sample.temp_act = 150;
+# sample.temp_avg = 124;
+# sample.temp_max = 324;
+# sample.temp_min = -224;
+# sample.temp_max_ts_offset = 224;
+# sample.temp_min_ts_offset = 24;
+# sample.humidity_act = 33;
+# sample.humidity_avg = 45;
+# sample.humidity_max = 15;
+# sample.humidity_min = 85;
+# sample.humidity_max_ts_offset = 32;
+# sample.humidity_min_ts_offset = 333;
+# sample.battery_voltage = 368;
+    
+packet = bytearray()
+packet.extend(b'\x01\x10\x21\x32\xa0\xfa\x03\x50\xd4\x12\x00\x00\x00\x00\x00')
+packet.extend(b'\x96\x00\x7c\x00\x44\x01\x20\xff\xe0\x00\x18\x00\x21\x00\x2d')
+packet.extend(b'\x0f\x55\x20\x00\x4d\x01\x70\x01')
+
+packet_dict = {
+    "version": packet[0],
+    "id": "%02X:%02X:%02X:%02X:%02X:%02X" % (packet[1], packet[2], packet[3], packet[4], packet[5], packet[6]),
+    "ts": struct.unpack('<q', packet[7:15])[0],
+    "temp_act": struct.unpack('<h', packet[15:17])[0],
+    "temp_avg": struct.unpack('<h', packet[17:19])[0],
+    "temp_max": struct.unpack('<h', packet[19:21])[0],
+    "temp_min": struct.unpack('<h', packet[21:23])[0],
+    "temp_max_ts_offset": struct.unpack('<h', packet[23:25])[0],
+    "temp_min_ts_offset": struct.unpack('<h', packet[25:27])[0],
+    "humidity_act": packet[27],
+    "humidity_avg": packet[28],
+    "humidity_max": packet[29],
+    "humidity_min": packet[30],
+    "humidity_max_ts_offset": struct.unpack('<h', packet[31:33])[0],
+    "humidity_min_ts_offset": struct.unpack('<h', packet[33:35])[0],
+    "battery_voltage": struct.unpack('<h', packet[35:37])[0],
+}
+print(packet_dict)
+
+# const char *DHTJsonDataTemplate =
+#     "{\"UID\":\"%s\",\"TS\":%d,\"SERIE\":{\"Temperature\":"
+#     "{\"f_avg\":%s,\"f_act\":%s,\"f_min\":%s,\"f_max\":%s,"
+#     "\"i_min_ts\":%u,\"i_max_ts\":%u},"
+#     "\"Humidity\":{\"f_avg\":%s,\"f_act\":%s,\"f_min\":%s,"
+#     "\"f_max\":%s,\"i_min_ts\":%u,\"i_max_ts\":%u}}}";
+packet_wlab_dict = {
+    "UID": packet_dict["id"],
+    "TS": packet_dict["ts"],
+    "SERIE": {
+        "Temperature": {
+            "f_avg": "%.1f" % (packet_dict["temp_avg"]/10),
+            "f_act": "%.1f" % (packet_dict["temp_act"]/10),
+            "f_min": "%.1f" % (packet_dict["temp_min"]/10),
+            "f_max": "%.1f" % (packet_dict["temp_max"]/10),
+            "i_min_ts": packet_dict["ts"] + packet_dict["temp_min_ts_offset"],
+            "i_max_ts": packet_dict["ts"] + packet_dict["temp_max_ts_offset"],
+        },
+        "Humidity": {
+            "f_avg": "%.1f" % (packet_dict["temp_avg"]/10),
+            "f_act": "%.1f" % (packet_dict["temp_act"]/10),
+            "f_min": "%.1f" % (packet_dict["temp_min"]/10),
+            "f_max": "%.1f" % (packet_dict["temp_max"]/10),
+            "i_min_ts": packet_dict["ts"] + packet_dict["humidity_min_ts_offset"],
+            "i_max_ts": packet_dict["ts"] + packet_dict["humidity_max_ts_offset"],
+        }
+    }
+}
+
+print(packet_wlab_dict)
